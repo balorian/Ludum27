9aef81bb1fe68278c852bdb676c1e51e3fcb047d
Mika Vuori <mika.vuori@bitwise.fi>
2013-08-25 01:50:39 +0300

    Added UI text

--- common/src/main/scala/com/car/l/Assets.scala
+++ common/src/main/scala/com/car/l/Assets.scala
@@ -2,6 +2,10 @@ package com.car.l
 
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 
 object Assets {
   val assets = new Assets
@@ -12,6 +16,15 @@ class Assets {
 
   lazy val uiAtlas = manager.get("images/ui.pack", classOf[TextureAtlas])
   lazy val creatureAtlas = manager.get("images/creatures.pack", classOf[TextureAtlas])
+  val font: BitmapFont = new BitmapFont(Gdx.files.classpath("images/ronda32_bold.fnt"), false)
+
+  val skin: Skin = {
+    val skin = new Skin()
+    val ls = new Label.LabelStyle()
+    ls.font = font
+    skin.add("default", ls)
+    skin
+  }
 
   def loadAll() = {
     manager.load("images/ui.pack", classOf[TextureAtlas])
--- common/src/main/scala/com/car/ui/GameUI.scala
+++ common/src/main/scala/com/car/ui/GameUI.scala
@@ -3,23 +3,43 @@ package com.car.ui
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.Gdx.graphics
 import com.badlogic.gdx.graphics.Color
+import com.car.l.Assets.assets
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+
+object GameUI {
+  val LEVEL = "Level - "
+  val SCORE = "Score - "
+}
 
 class GameUI {
   lazy val stage: Stage = new Stage(graphics.getWidth, graphics.getHeight,
     false)
 
+  val font = assets.font
+
   val spiritBar = new StateBar(10, 10, 64, graphics.getHeight() - 20, new Color(1f, 0xFF / 0x13, 0xFF / 0x7F, 1))
   val healthBar = new StateBar(graphics.getWidth() - 74, 10, 64, graphics.getHeight() - 20, new Color(0.75f, 0, 0, 1))
 
+  val scoreLabel = new Label(GameUI.SCORE + "2000", assets.skin)
+  scoreLabel.setPosition(100, graphics.getHeight() - 50)
+  
+  val levelLabel = new Label(GameUI.LEVEL + "1", assets.skin)
+  levelLabel.setPosition(graphics.getWidth() - 100f - font.getBounds(levelLabel.getText()).width, graphics.getHeight() - 50)
+  
+
   stage.addActor(spiritBar)
   stage.addActor(healthBar)
+  stage.addActor(scoreLabel)
+  stage.addActor(levelLabel)
 
   def update() {
-
+    levelLabel.setText(GameUI.LEVEL + "1")
+    scoreLabel.setText(GameUI.SCORE + "2000")
   }
 
   def render(delta: Float) {
     stage.act(delta)
     stage.draw()
+
   }
 }
